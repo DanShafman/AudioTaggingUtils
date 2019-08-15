7c2da37e10a25954bdbc7e8e0b9a28e31ca8f0c8
DanShafman <dshafman0900@bths.edu>
2019-08-14 23:34:38 -0400

    Added detach_audio function

--- tagging_utils.py
+++ tagging_utils.py
@@ -12,3 +12,12 @@ def augment_clip(input_clip_filename, augment_audio_filename, output_filename, s
     input_clip.audio = audio_clip_aug
 
     input_clip.write_videofile(output_filename, audio_codec=aud_codec)
+
+def detach_audio(input_clip_filename):
+    filename_raw = input_clip_filename.split('.')[0]
+    input_clip = VideoFileClip(input_clip_filename)
+    audio_clip = input_clip.audio
+
+    audio_clip.write_audiofile(filename_raw + ".wav", 44100)
+
+detach_audio("58clip.mp4")
